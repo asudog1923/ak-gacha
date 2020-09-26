fe9dacb857e2b9121b687fcda451e75a03733a4e
Royhan <60024250+ookamiiixd@users.noreply.github.com>
2020-09-27 01:41:01 +0700

    minimized the required display

--- index.php
+++ index.php
@@ -35,7 +35,7 @@
 	</script>
 	</div>
 	<div id="zaBulokku">
-		<p>Minimum device width is 850 pixels and landscape view on mobile in order to give you the best experince, well yea the ui still suck tho...lol.</p>
+		<p>Minimum device width is 600 pixels and landscape view on mobile in order to give you the best experince, well yea the ui still suck tho...lol.</p>
 	</div>
 	<footer>
 		Find me on <a href="https://github.com/ookamiiixd/ak-gacha" target="_blank">github</a>.
